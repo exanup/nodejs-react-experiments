5bc776dacb35e75188bd87dfb8f31678e961e9a4
Anup Dhakal <anupdhakal2000@gmail.com>
2018-07-16 22:40:11 +0545

    Enable auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,26 @@
-// const Boom = require('boom');
+const Boom = require('boom');
 
-// const jwtHelper = require('../utils/jwt');
+const jwtHelper = require('../utils/jwt');
 const User = require('../models/User');
 
 async function authenticate(req, res, next) {
   // for now let's make it such that a user is automatically logged in
-  const userIdToLogin = 1;
-  const user = await User.fetchById(userIdToLogin);
-  req.user = user;
-  next();
+  // const userIdToLogin = 1;
+  // const user = await User.fetchById(userIdToLogin);
+  // req.user = user;
+  // next();
 
-  // const accessToken = req.get('Authorization');
+  const accessToken = req.get('Authorization');
 
-  // try {
-  //   const userId = jwtHelper.verifyAccessToken(accessToken).data;
-  //   const user = await User.fetchById(userId);
-  //   req.user = user;
+  try {
+    const userId = jwtHelper.verifyAccessToken(accessToken).data;
+    const user = await User.fetchById(userId);
+    req.user = user;
 
-  //   next();
-  // } catch (err) {
-  //   next(Boom.unauthorized());
-  // }
+    next();
+  } catch (err) {
+    next(Boom.unauthorized());
+  }
 }
 
 module.exports = authenticate;
